6226da2f56afe20a567db12fea806f626fe5ecdf
saurabhthesuperhero <saurabhthesuperhero@gmail.com>
2020-08-18 00:30:50 +0530

    rewuirement

--- hello.py
+++ hello.py
@@ -29,5 +29,5 @@ def hello_world(lstring):
     return jsonify(dictionary)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+	port = int(os.environ.get("PORT", 5000))
+	app.run(debug=True, host='0.0.0.0', port=port)
