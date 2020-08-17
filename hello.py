052930a7e082f87e18fc664fca177c9bfbd58d7f
saurabhthesuperhero <saurabhthesuperhero@gmail.com>
2020-08-17 23:18:01 +0530

    api done

---
+++ hello.py
@@ -0,0 +1,29 @@
+from flask import Flask,jsonify
+import bs4
+import requests
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/<lstring>')
+
+def hello_world(lstring):
+    #lstring='Perfect ed sheeran'
+    res=requests.get('https://search.azlyrics.com/search.php?q=%s'%lstring)
+    soup=bs4.BeautifulSoup(res.text,'lxml')
+    result=soup.find_all('td',{"class":"text-left"})
+    i=1
+    global x
+    x=['0']*10
+    y=[]
+    col_link=[]
+
+    for link in result [0:5]:
+        #print(link.text)
+        col_link.append(link.text) 
+        y.append(link.find('a').get('href'))
+    
+        # i=i+1
+    string_list="<br>".join(str(i) for i in col_link)
+
+    dictionary=dict(zip(col_link,y))
+    return jsonify(dictionary)
