d1879848afea2f4621ed7133b6110dd41db9044e
saurabhthesuperhero <saurabhthesuperhero@gmail.com>
2020-08-19 00:24:58 +0530

    api format change

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask,jsonify
+from flask import Flask,jsonify,render_template
 import bs4
 import requests
 app = Flask(__name__)
@@ -13,28 +13,12 @@ def hello_world(lstring):
     res=requests.get('https://search.azlyrics.com/search.php?q=%s'%lstring,timeout=5)
     soup=bs4.BeautifulSoup(res.text,'lxml')
     result=soup.find_all('td',{"class":"text-left"})
-    y=[]
-    global col_link 
-    col_link=[]
-
+    data=[]
     for link in result [0:5]:
-        #print(link.text)
         
         temp=(str(link.text[3:]).find('\n'))#ignoring first \n and will stop after second \n
-        # -creepy but good
-       # print(link.text[3:temp+3])
-
-        
-        col_link.append(link.text[3:temp+3]) 
-        y.append(link.find('a').get('href'))
-
-  
-    
-        # i=i+1
-    string_list="<br>".join(str(i) for i in col_link)
-
-    dictionary=dict(zip(col_link,y))
-    return jsonify(dictionary)
+        data.append({"name":link.text[3:temp+3],"link":link.find('a').get('href')})
+    return jsonify(data=data,status=200)
     
 
 
@@ -53,5 +37,27 @@ def showlyrics(input_url):
 
 
     lindex=lyricss.find('Submit Corrections')
-    return jsonify(lyricss[10:lindex])
+    data=[{"data":lyricss[10:lindex]}]
+    return jsonify(data=data,status=200)
+
+
+# now creating demo app so we will call api 
+
+def callapi():
+	import requests,json
+
+	url='http://127.0.0.1:5000/search=blank%20space'
+
+	res=requests.get(url)
+	res=json.loads(res.content)
+	return res['data']
+
+
+
+@app.route('/demo')
+def demo():
+	listlyrics=callapi()
+	print(listlyrics)
+	return render_template('lyricui.html',listlyric=listlyrics)
+
 
