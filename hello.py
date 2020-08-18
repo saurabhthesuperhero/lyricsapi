4d26a6a6538e6ef9a7644ae8f7e87682701b3ab4
saurabhthesuperhero <saurabhthesuperhero@gmail.com>
2020-08-18 08:58:41 +0530

    oh

--- hello.py
+++ hello.py
@@ -25,5 +25,5 @@ def hello_world(lstring):
     string_list="<br>".join(str(i) for i in col_link)
 
     dictionary=dict(zip(col_link,y))
-    return jsonify(dictionary)
+    return jsonify(string_list)
 
