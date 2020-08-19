d62c402b1fce311613e30b39b039b0a518e55954
saurabhthesuperhero <saurabhthesuperhero@gmail.com>
2020-08-19 15:06:03 +0530

    (stable version1 working done)

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 from flask import Flask,jsonify,render_template
 import bs4
-import requests
+import requests,json
 app = Flask(__name__)
 
 @app.route('/')
@@ -14,14 +14,14 @@ def hello_world(lstring):
     soup=bs4.BeautifulSoup(res.text,'lxml')
     result=soup.find_all('td',{"class":"text-left"})
     data=[]
-    for link in result [0:5]:
+    for link in result [0:6]:
         
         temp=(str(link.text[3:]).find('\n'))#ignoring first \n and will stop after second \n
         data.append({"name":link.text[3:temp+3],"link":link.find('a').get('href')})
     return jsonify(data=data,status=200)
     
 
-
+#api
 @app.route('/link=<path:input_url>')
 def showlyrics(input_url):
 
@@ -43,6 +43,24 @@ def showlyrics(input_url):
 
 # now creating demo app so we will call api 
 
+
+def showlyricsnotapi(input_url):
+
+    #linkss=int(input("Enter song number ........"))
+    print("hello")
+    lres=requests.get(input_url)
+    lsoup=bs4.BeautifulSoup(lres.text,'lxml')
+
+    lresult=lsoup.find_all('div',{'class':'col-xs-12'})
+    check='<!-- MxM banner -->'
+    for lyric in lresult [1:2]:
+        lyricss=lyric.text
+
+
+    lindex=lyricss.find('Submit Corrections')
+    data=[{"data":lyricss[10:lindex]}]
+    return (data)
+
 def callapi():
 	import requests,json
 
@@ -52,12 +70,29 @@ def callapi():
 	res=json.loads(res.content)
 	return res['data']
 
-
-
 @app.route('/demo')
 def demo():
 	listlyrics=callapi()
 	print(listlyrics)
-	return render_template('lyricui.html',listlyric=listlyrics)
+	return render_template('lyricui.html')
+
+
+@app.route('/demo/view=<path:input_url>',methods=['GET', 'POST'])
+def viewlyric(input_url):
+    x=showlyricsnotapi(input_url)
+    xx=x[0]['data']
+    xx=(xx.replace("\n\n\n\n\n\n\n",""))
+    # print(xx)
+    xx=xx.replace("\n\n\n\n\n\n","\n")
+    xx=xx.replace("\n","</br>")
+    temp=xx.find("lyrics")
+    nn=(xx[:temp])
+    xx=xx[temp+6:]
+    print( str(xx))
+    return render_template('viewlyric.html',name=nn,lyric=str(xx))
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return "<div style='text-align:center;padding:30;'><h1>Url Doesnt exist, hahaha!</h1></div>", 404
 
 
